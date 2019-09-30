fed2cd167d5a61b53c3d74ac3db61d2930a3ca6b
Ochieng__ <jrhosea8@gmail.com>
2019-09-30 08:35:59 +0300

    Initial commit

---
+++ app/src/main/java/com/hosea/ochieng/eldoradoschool/MainActivity.java
@@ -0,0 +1,32 @@
+package com.hosea.ochieng.eldoradoschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://mopatech.co.ke/eldorado/site/userlogin");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
